8a11e578b5c2fc8f8a6053a1248df355f93ec716
Asherpayn <147255790+Asherpayn@users.noreply.github.com>
2023-10-27 21:37:39 +0100

    ugh

--- python1.py
+++ python1.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 print("hello world")
 print("bubber blubber blubber")
-=======
-print("hello world")
-print("blubber blubber blubber")
->>>>>>> 7b5f6035a96656a713aca6e49ba823e9204cf99e
