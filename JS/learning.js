1c69801f4eeed1bf402b5360b310079f7422693a
Asher payn <asherpayn@outlook.com>
2024-09-29 14:07:59 +0100

    "Added comments and new code to learning.js, including variable declarations, function definitions, and console logs."

--- JS/learning.js
+++ JS/learning.js
@@ -1,1 +1,20 @@
-let age = 11
+//i have no clue what i am doing 😀
+
+
+let age = 11 //creating a variable called age
+let adultage = 18 
+console.log(age) //basically print from python
+
+function double(number) { //creating a function called double that takes in a number and returns the doubled number
+    return number * 2
+}
+
+function subtract(number, number2) { //creating a function called subtract that takes in two numbers and returns the subtracted number
+    return number - number2
+}
+
+let doubledage = double(age) //creates a variable that calls the double function to double the variable "age"
+console.log(doubledage)
+
+let yearstoadult = subtract(adultage, age) //creates a variable called years to adult that calls the subtract function to subtract age from adultage
+console.log(yearstoadult)
