982cd9680c584d5e85b8f3b75a61f113e91557ee
Asher payn <asherpayn@gmail.com>
2025-05-31 22:11:37 +0100

    Some changes to wording and a small fix

--- JS/html_tests.js
+++ JS/html_tests.js
@@ -1,18 +1,16 @@
-alert('Welcome');
-let username = prompt("What is your name?");
+alert("Welcome");
+let food = prompt("What is your favourite Food?");
 
+document.getElementsByTagName("h1")[0].onmouseover = function () {
+  this.innerText = "MOUSE IS OVER";
+  this.style.color = "Blue";
+};
 
+document.getElementsByTagName("h1")[0].onmouseout = function () {
+  this.innerText = "I love the portal games";
+  this.style.color = "aqua";
+};
 
-document.getElementsByTagName('h1')[0].onmouseover = function() {
-    this.innerText = 'MOUSE IS OVER';
-    this.style.color = "Blue";
-}
-
-document.getElementsByTagName('h1')[0].onmouseout = function() {
-    this.innerText = 'I love the portal games';
-    this.style.color = "aqua";
-}
-
-ducument.getElementsByTagName('button')[0].onclick = function() {
-    this.innerText = username
-}
+document.getElementsByTagName("button")[0].onclick = function () {
+  this.innerText = food;
+};
